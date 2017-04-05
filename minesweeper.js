9b1b92262381abfed9c28a49bd0e80a7c16c374a
Alexei Boronine <alexei@boronine.com>
2017-04-06 00:55:10 +0800

    Add click event listener

--- minesweeper.js
+++ minesweeper.js
@@ -1,10 +1,22 @@
-function main() {
+document.addEventListener('DOMContentLoaded', function () {
     const canvasElement = document.getElementById('main-canvas');
+    const context = canvasElement.getContext('2d');
     canvasElement.width = 200;
     canvasElement.height = 200;
-    const context = canvasElement.getContext('2d');
+
     context.fillStyle = 'green';
     context.fillRect(0, 0, 200, 200);
-}
 
-document.addEventListener('DOMContentLoaded', main);
+    canvasElement.addEventListener("click", function (event) {
+        event.preventDefault();
+        const canvasRect = canvasElement.getBoundingClientRect();
+        const x = event.clientX - canvasRect.left;
+        const y = event.clientY - canvasRect.top;
+        console.log('Left click!', x, y);
+    });
+
+    canvasElement.addEventListener("contextmenu", function (event) {
+        event.preventDefault();
+        console.log('Right click!');
+    });
+});
